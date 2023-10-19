3afe029f9b412fab02c56fd9c26464f7c1498149
CodingArco <codingarco@gmail.com>
2023-10-19 13:30:06 +0200

    added implementation for luminosity (and lightSensitivity)

--- CodesContainer.java
+++ CodesContainer.java
@@ -6,7 +6,7 @@ class CodesContainer extends AbstractList<Code>
     private final Code[] a;
     public int averageHeatResistance = 0;
     public int averageColdResistance = 0;
-    public int averageLuminosity = 0;
+    public int averageLightSensitivity = 0;
     public int averageMaxEnergy = 0;
     public int averageMaxDamage = 0;
 
@@ -37,29 +37,34 @@ class CodesContainer extends AbstractList<Code>
     void updateAverages() {
         int totalHeatResistance = 0;
         int totalColdResistance = 0;
+        int totalLightSensitivity = 0;
         int totalMaxEnergy = 0;
         int totalMaxDamage = 0;
         for (Code code : a) {
             totalHeatResistance += code.heatResistance;
             totalColdResistance += code.coldResistance;
+            totalLightSensitivity += code.lightSensitivity;
             totalMaxEnergy += code.maxEnergy;
             totalMaxDamage += code.maxDamage;
         }
         averageHeatResistance = totalHeatResistance / a.length;
         averageColdResistance = totalColdResistance / a.length;
+        averageLightSensitivity = totalLightSensitivity / a.length;
         averageMaxEnergy = totalMaxEnergy / a.length;
         averageMaxDamage = totalMaxDamage / a.length;
     }
 
     public ProgressBarData[] getProgressBarData() {
         return new ProgressBarData[] {
-            new ProgressBarData("Heat resistance", 0, Code.maxValue,
+            new ProgressBarData("Heat resistance", 0, Code.MAX_VALUE,
                 averageHeatResistance, "°C"),
-            new ProgressBarData("Cold resistance", 0, Code.maxValue,
+            new ProgressBarData("Cold resistance", 0, Code.MAX_VALUE,
                 averageColdResistance, "°C"),
-            new ProgressBarData("Max energy", 0, Code.maxValue,
+            new ProgressBarData("Light sensitivity", 0, Code.MAX_VALUE,
+                averageLightSensitivity),
+            new ProgressBarData("Max energy", 0, Code.MAX_VALUE,
                 averageMaxEnergy),
-            new ProgressBarData("Max damage", 0, Code.maxValue,
+            new ProgressBarData("Max damage", 0, Code.MAX_VALUE,
                 averageMaxDamage),
         };
     }
